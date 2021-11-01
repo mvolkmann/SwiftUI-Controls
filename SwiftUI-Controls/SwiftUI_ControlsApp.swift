d30c894b2f335345382d84afce60c79852312341
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-01 05:41:45 -0500

    Initial Commit

---
+++ SwiftUI-Controls/SwiftUI_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ControlsApp.swift
+//  SwiftUI-Controls
+//
+//  Created by R. Mark Volkmann on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
